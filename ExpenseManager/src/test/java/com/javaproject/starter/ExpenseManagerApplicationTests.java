4988070acf732105633ab9b8e7d81ef4dac9e0f4
Parv17k <khatriparv@gmail.com>
2021-12-13 16:29:34 -0500

    Intial Setup

---
+++ ExpenseManager/src/test/java/com/javaproject/starter/ExpenseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
